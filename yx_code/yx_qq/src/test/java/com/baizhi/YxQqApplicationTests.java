6e81d18f52c1417901660912902acd65d86fba53
xueyw <396671762@qq.com>
2020-12-01 20:29:31 +0800

    这是第二次提交

---
+++ yx_code/yx_qq/src/test/java/com/baizhi/YxQqApplicationTests.java
@@ -0,0 +1,15 @@
+package com.baizhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YxQqApplicationTests {
+
+    @Test
+    void contextLoads() {
+        int aa = 10;
+        int bb = 20;
+    }
+
+}
